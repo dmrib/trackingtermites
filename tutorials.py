8be7537621b8ddf373a7729818d68444ca32a52b
dmrib <dmrib.cs@gmail.com>
2017-05-24 17:32:39 -0300

    Add tutorial no.2

--- tutorials.py
+++ tutorials.py
@@ -9,5 +9,22 @@ def tutorial_one():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+def tutorial_two():
+    cap = cv2.VideoCapture(-1)
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640,480))
+    while True:
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        out.write(frame)
+        cv2.imshow('frame', frame)
+        cv2.imshow('gray', gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
 if __name__ == '__main__':
-    tutorial_one()
+    tutorial_two()
