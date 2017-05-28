54e2e0967687df32835d013b953f329857e2b2bb
dmrib <dmrib.cs@gmail.com>
2017-05-28 14:46:30 -0300

    Add tutorial no.3

--- tutorials.py
+++ tutorials.py
@@ -26,5 +26,22 @@ def tutorial_two():
     out.release()
     cv2.destroyAllWindows()
 
+def tutorial_three():
+    img = cv2.imread('images/termite-01.jpg', cv2.IMREAD_COLOR)
+
+    cv2.line(img, (0, 0), (30, 150), (0, 0, 0), 5)
+    cv2.rectangle(img, (40,0), (60, 20), (0, 255, 0), 5)
+    cv2.circle(img, (60, 60), 30, (0, 0, 255), -1)
+
+    pts = np.array([[10,5], [40,21], [30,90]], np.int32)
+    cv2.polylines(img, [pts], True, (255, 255, 0), 3)
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(img, 'OpenCV', (0, 100), font, 1, (200, 120, 30), 2, cv2.LINE_AA)
+
+    cv2.imshow('img', img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
 if __name__ == '__main__':
-    tutorial_two()
+    tutorial_three()
