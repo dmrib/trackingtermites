36a24a03a8b32cae654eb3b9e3b009d04f34c713
dmrib <dmrib.cs@gmail.com>
2017-06-26 22:25:03 -0300

    Add tutorial no.9

--- tutorials.py
+++ tutorials.py
@@ -184,5 +184,52 @@ def tutorial_eight():
     plt.show()
 
 
+def tutorial_nine():
+    """Haar cascade face, eyes, smile and cat face detection."""
+    face_cascade = cv2.CascadeClassifier('haarcascades/'
+                                         'haarcascade_frontalface_alt_tree'
+                                         '.xml')
+    eye_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
+    smile_cascade = cv2.CascadeClassifier('haarcascades/'
+                                          'haarcascade_frontalcatface'
+                                          '_extended.xml')
+
+    cap = cv2.VideoCapture(0)
+
+    while 1:
+        ret, img = cap.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]
+
+            eyes = eye_cascade.detectMultiScale(roi_gray)
+            for (ex, ey, ew, eh) in eyes:
+                cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh),
+                              (0, 255, 0), 2)
+
+            smiles = smile_cascade.detectMultiScale(roi_gray)
+            for (sx, sy, sw, sh) in smiles:
+                cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh),
+                              (0, 0, 255), 2)
+
+        cats = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x, y, w, h) in cats:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 255, 0), 2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]
+
+        cv2.imshow('img', img)
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
 if __name__ == '__main__':
-    tutorial_eight()
+    tutorial_nine()
