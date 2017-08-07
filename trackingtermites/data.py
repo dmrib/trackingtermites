c279a5b3c83f6f038a8cb11955f9a54edb7b46e2
dmrib <dmrib.cs@gmail.com>
2017-08-07 11:06:40 -0300

    Created input file parser

--- trackingtermites/data.py
+++ trackingtermites/data.py
@@ -12,6 +12,8 @@ class DataHandler:
         Returns:
             None.
         """
+        self.input_path = input_path
+        self.output_path = output_path
 
     def load_input(self):
         """Read input file and creates parameters dictionary.
@@ -21,7 +23,26 @@ class DataHandler:
         Returns:
             parameters (dict): experiment parameters.
         """
+        parameters = {}
+        with open(self.input_path, mode='r', encoding='utf-8') as input_file:
+            for line in input_file:
+                if not line[0] == '\n' and not line[0] == '#' and not line[0] == ' ':
+                    param, value = line.rstrip('\n').split(' ')
+                    parameters[param] = value
 
+        if 'n_termites' in parameters:
+            parameters['n_termites'] = int(parameters['n_termites'])
+
+        if 'box_size' in parameters:
+            parameters['box_size'] = int(parameters['box_size'])
+
+        if 'video_source_size' in parameters:
+            y, x = parameters['video_source_size'].rstrip('\n').split(',')
+            parameters['video_source_size'] = tuple([int(y), int(x)])
+
+
+
+        print(parameters)
 
     def write_output(self):
         """Write output data to file.
@@ -31,3 +52,9 @@ class DataHandler:
         Returns:
             None.
         """
+        pass
+
+
+if __name__ == '__main__':
+    handler = DataHandler('../data/sample_input.txt', '')
+    handler.load_input()
