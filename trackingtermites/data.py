be0af1b85606132eaab2a0357fb3b2c26c9ffc8f
dmrib <dmrib.cs@gmail.com>
2017-08-22 08:37:48 -0300

    Include 'distances' column in output

--- trackingtermites/data.py
+++ trackingtermites/data.py
@@ -75,7 +75,7 @@ class DataHandler:
                 out_file.write(f'# Termite number: {termite.identity}\n')
                 out_file.write(f'# Color: {termite.color}\n\n')
                 out_file.write('###\n\n')
-                out_file.write('frame, y, x, colliding\n')
+                out_file.write('frame, y, x, colliding, distances\n')
                 for frame, location in enumerate(termite.path):
                     out_file.write(f'{frame}, {location[0]}, {location[1]}, {location[2]}, {location[3]}\n')
 
