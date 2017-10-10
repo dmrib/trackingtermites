ee30d1f8b268f6a5862de0527bc48dd45f59b9f6
dmrib <dmrib.cs@gmail.com>
2017-10-10 17:43:47 -0300

    Add termite radius to integer parameters

--- trackingtermites/utils.py
+++ trackingtermites/utils.py
@@ -6,7 +6,7 @@ boolean_parameters = ['show_labels', 'highlight_collisions',
 
 tuple_parameters = ['video_source_size', 'arena_size']
 
-integer_parameters = ['n_termites', 'box_size', 'scale', 'trail_size']
+integer_parameters = ['n_termites', 'box_size', 'scale', 'trail_size', 'termite_radius']
 
 list_parameters = ['filters']
 
