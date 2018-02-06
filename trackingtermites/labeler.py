ea5fb9cc4719593834aa7243dfa67c603bd3c4b3
dmrib <dmrib.cs@gmail.com>
2018-02-06 18:09:18 -0200

    No redundant training examples

--- trackingtermites/labeler.py
+++ trackingtermites/labeler.py
@@ -25,9 +25,9 @@ class LabelingSession():
 
         if not os.path.exists(self.output_path):
             os.makedirs(self.output_path)
-            os.makedirs(os.path.join(self.output_path, 'proctodeal'))
-            os.makedirs(os.path.join(self.output_path, 'stomatodeal'))
-            os.makedirs(os.path.join(self.output_path, 'no-trophallaxis'))
+            os.makedirs(os.path.join(self.output_path, 'head-head'))
+            os.makedirs(os.path.join(self.output_path, 'head-abdomen'))
+            os.makedirs(os.path.join(self.output_path, 'no-interaction'))
 
         self.video = cv2.VideoCapture(self.metadata['video_path'])
 
@@ -87,42 +87,41 @@ class LabelingSession():
         for frame_number in range(1, len(self.termites[0].trail['frame'])):
             playing, frame = self.video.read()
             frame = cv2.resize(frame, (0,0), fx=self.metadata['resize_ratio'],
-                       fy=self.metadata['resize_ratio'])
+                               fy=self.metadata['resize_ratio'])
             clear = frame.copy()
 
-            for termite in self.termites:
-                predicted = (int(termite.trail.loc[frame_number, 'x'] + (termite.trail.loc[frame_number, 'xoffset']/2)),
-                             int(termite.trail.loc[frame_number, 'y'] + (termite.trail.loc[frame_number, 'yoffset']/2)))
-                cv2.circle(frame, predicted, 3, termite.color, -1)
-                cv2.putText(frame, termite.trail.loc[frame_number,'label'], (predicted[0]+5, predicted[1]+5), 2,
-                            color=termite.color, fontScale=0.3)
+            for n_termite in range(len(self.termites)):
+                predicted = (int(self.termites[n_termite].trail.loc[frame_number, 'x'] + (self.termites[n_termite].trail.loc[frame_number, 'xoffset']/2)),
+                             int(self.termites[n_termite].trail.loc[frame_number, 'y'] + (self.termites[n_termite].trail.loc[frame_number, 'yoffset']/2)))
+                cv2.circle(frame, predicted, 3, self.termites[n_termite].color, -1)
+                cv2.putText(frame, self.termites[n_termite].trail.loc[frame_number,'label'], (predicted[0]+5, predicted[1]+5), 2,
+                            color=self.termites[n_termite].color, fontScale=0.3)
 
-                for other in self.termites:
-                    if other.trail.loc[0, 'label'] != termite.trail.loc[0, 'label']:
-                        other_predicted = (int(other.trail.loc[frame_number, 'x'] + (other.trail.loc[frame_number, 'xoffset']/2)),
-                                           int(other.trail.loc[frame_number, 'y'] + (other.trail.loc[frame_number, 'yoffset']/2)))
-                        if termite.trail.loc[frame_number, 'distance_to_{}'.format(other.trail.loc[0, 'label'])] < 60:
-                            cv2.line(frame, predicted, other_predicted, (0,0,255), 1)
-                            half = ((predicted[0]+other_predicted[0])//2, (predicted[1]+other_predicted[1])//2)
-                            cv2.circle(frame, half, 3, (255, 0, 0), -1)
+                for other in range(n_termite+1, len(self.termites)):
+                    other_predicted = (int(self.termites[other].trail.loc[frame_number, 'x'] + (self.termites[other].trail.loc[frame_number, 'xoffset']/2)),
+                                       int(self.termites[other].trail.loc[frame_number, 'y'] + (self.termites[other].trail.loc[frame_number, 'yoffset']/2)))
+                    if self.termites[n_termite].trail.loc[frame_number, 'distance_to_{}'.format(self.termites[other].trail.loc[0, 'label'])] < 60:
+                        cv2.line(frame, predicted, other_predicted, (0,0,255), 1)
+                        half = ((predicted[0]+other_predicted[0])//2, (predicted[1]+other_predicted[1])//2)
+                        cv2.circle(frame, half, 3, (255, 0, 0), -1)
 
-                            event = clear[(half[1]-25):(half[1]+25), (half[0]-25):(half[0]+25)]
-                            cv2.imshow('Encounter', event)
+                        event = clear[(half[1]-25):(half[1]+25), (half[0]-25):(half[0]+25)]
+                        cv2.imshow('Encounter', event)
 
-                            encouter_label = cv2.waitKey(0) & 0xff
-                            if encouter_label == 27:
-                                sys.exit()
-                            elif encouter_label == ord('q'):
-                                cv2.imwrite(os.path.join(self.output_path, 'proctodeal/{}-{}-proctodeal.jpg'.format(termite.trail.loc[0, 'label'],
-                                                                            frame_number)), event)
-                            elif encouter_label == ord('w'):
-                                cv2.imwrite(os.path.join(self.output_path, 'stomatodeal/{}-{}-stomatodeal.jpg'.format(termite.trail.loc[0, 'label'],
-                                                                            frame_number)), event)
-                            elif encouter_label == ord('e'):
-                                cv2.imwrite(os.path.join(self.output_path, 'no-trophallaxis/{}-{}-no-trophallaxis.jpg'.format(termite.trail.loc[0, 'label'],
-                                                                            frame_number)), event)
+                        encouter_label = cv2.waitKey(0) & 0xff
+                        if encouter_label == 27:
+                            sys.exit()
+                        elif encouter_label == ord('q'):
+                            cv2.imwrite(os.path.join(self.output_path, 'head-head/{}-{}-head-head.jpg'.format(self.termites[n_termite].trail.loc[0, 'label'],
+                                                                        frame_number)), event)
+                        elif encouter_label == ord('w'):
+                            cv2.imwrite(os.path.join(self.output_path, 'head-abdomen/{}-{}-head-abdomen.jpg'.format(self.termites[n_termite].trail.loc[0, 'label'],
+                                                                        frame_number)), event)
+                        elif encouter_label == ord('e'):
+                            cv2.imwrite(os.path.join(self.output_path, 'no-interaction/{}-{}-no-interaction.jpg'.format(self.termites[n_termite].trail.loc[0, 'label'],
+                                                                        frame_number)), event)
 
-                            cv2.destroyWindow('Encounter')
+                        cv2.destroyWindow('Encounter')
 
             cv2.imshow('Labeling...', frame)
             pressed_key = cv2.waitKey(10) & 0xff
