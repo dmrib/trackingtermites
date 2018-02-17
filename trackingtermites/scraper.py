7b658b0369473eea7cc8a5af10c7b22ba2c71751
dmrib <dmrib.cs@gmail.com>
2018-02-17 13:39:55 -0200

    Revisting labelers

--- trackingtermites/scraper.py
+++ trackingtermites/scraper.py
@@ -23,7 +23,6 @@ class Scraper():
         self._load_metadata()
         self._load_termites()
 
-
         self.video = cv2.VideoCapture(self.metadata['video_path'])
 
     def _load_metadata(self):
@@ -120,102 +119,6 @@ class Scraper():
                         cv2.imwrite(self.settings['output_path']+'{}-t{}-t{}.jpg'.format(frame_number, n_termite, other), event)
 
 
-class OfflineLabeler():
-    def __init__(self, settings_file):
-        '''Initializer.
-
-        Args:
-            settings_path (str): path to settings file.
-        Returns:
-            None.
-        '''
-        self.paths = []
-        self._load_settings(settings_file)
-        self.collect_paths()
-
-    def _load_settings(self, settings_file):
-        '''Load labeling session settings file.
-
-        Args:
-            settings_file (str): path to tracking session settings file.
-        Retuns:
-            None.
-        '''
-        with open(settings_file) as settings:
-            self.settings = json.load(settings)
-
-    def _create_collections(self):
-        '''Create labeled images folders.
-
-        Args:
-            None.
-        Returns:
-            None.
-        '''
-        for event in self.settings['events'].values():
-            folder_name = os.path.join(self.settings['images_folder'], event)
-            if not os.path.exists(folder_name):
-                os.makedirs(folder_name)
-
-    def collect_paths(self):
-        '''Get all images paths in the images folder.
-
-        Args:
-            None.
-        Returns:
-            None.
-        '''
-        self.paths = [x for x in glob.glob(self.settings['images_folder']+'*.jpg')]
-
-    def augment_dataset(self):
-        '''Artificially augment dataset.
-
-        Args:
-            None.
-        Returns:
-            None.
-        '''
-        print('Augmenting dataset...')
-        for folder in self.settings['events'].values():
-            labeled = glob.glob(os.path.join(self.settings['images_folder'],
-                                folder + '/*.jpg'))
-            for image in labeled:
-                frame = cv2.imread(image)
-                artificial = cv2.flip(frame, 0)
-                cv2.imwrite(image[:-4]+'-a1'+image[-4:], artificial)
-                artificial = cv2.flip(frame, 1)
-                cv2.imwrite(image[:-4]+'-a2'+image[-4:], artificial)
-                artificial = cv2.flip(artificial, 0)
-                cv2.imwrite(image[:-4]+'-a3'+image[-4:], artificial)
-
-    def label(self):
-        '''Start hand labeling loop.
-
-        Args:
-            None.
-        Returns:
-            None.
-        '''
-        self._create_collections()
-        for image_id, path in enumerate(self.paths):
-            print(path)
-            frame = cv2.imread(path)
-            cv2.imshow('Labeling', frame)
-            pressed_key = cv2.waitKey(0) & 0xff
-            if chr(pressed_key) in self.settings['events']:
-                path = os.path.join(self.settings['images_folder'],
-                                    self.settings['events'][chr(pressed_key)] +
-                                    '/' + str(image_id) + '.jpg')
-                cv2.imwrite(path, frame)
-
-            if pressed_key == 27:
-                sys.exit()
-
-
 if __name__ == '__main__':
     scraper = Scraper('settings/scraper.json')
     scraper.scrape()
-
-    labeler = OfflineLabeler('settings/scraper.json')
-    labeler.label()
-    labeler.augment_dataset()
